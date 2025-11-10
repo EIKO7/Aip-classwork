717531b213d132d4f70a926cb0e31632d0bf5724
EIKO7 <camper1773@mail.ru>
2025-11-10 12:56:12 +0300

    ver 3

--- main1.cpp
+++ main1.cpp
@@ -22,13 +22,77 @@ IntArray a;
 
 a.add(next);
 
-//ввод
 
-for (size_t i = 0, i < a.size(); ++i)
+while (std::cin >> next)
 {
 
-  int d = a.get(i);
+ a.add(next);
 
 }
+
+if (!std:cin)
+{#include <iostream>
+
+struct IntArray
+{
+ IntArray(int i)
+ {
+
+ }
+
+ ~IntArray()
+ {
+
+ }
+
+ int add(int i);
+ int get(size_t id) const;
+ size_t size() const;
+ int last() const;
+};
+
+int main()
+{
+ int next = 0;
+ std::cin >> next;
+
+ IntArray a(next);
+
+ while (std::cin >> next)
+ {
+  a.add(next);
+ }
+
+ if (!std::cin)
+ {
+  return 1;
+ }
+
+ size_t count = 1;
+
+ for (size_t i = 0; i < a.size() - 1; ++i)
+ {
+  int d = a.get(i);
+ 
+  count += !(d % a.last()) ? 1 : 0;
+ }
+
+ std::cout << count << "\n";
+}
+
+  return 1;
+
+}
+
+
+for (size_t i = 0, i < a.size(); - 1; ++i)
+   {
+
+       int d = a.get(i);
+
+       const += !(d % a.last()) ? 1 : 0;
+
+   }
+ std::cout << count << "\n";
 
 }
