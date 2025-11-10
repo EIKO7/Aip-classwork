cf503c337ecd4338238f17382ec55669b19e71f8
EIKO7 <camper1773@mail.ru>
2025-11-10 12:44:23 +0300

    ver 2

--- main1.cpp
+++ main1.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 
+struct IntArray
+{
+   int add(int i);
+   int get (size_t id) const;
+   size_t size() const;
+
+};
+
+
 int main()
 {
 int next = 0;
 std::cin >> next;
-for (!std::cin){
+
+if (!std::cin){
   return 1;
 
-  }
+
+IntArray a;
+
+a.add(next);
+
+//ввод
+
+for (size_t i = 0, i < a.size(); ++i)
+{
+
+  int d = a.get(i);
+
+}
+
 }
