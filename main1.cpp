dd78c748487d6ebbdabf8e4d6fb6d7e739dc5c05
EIKO7 <camper1773@mail.ru>
2025-11-10 13:01:43 +0300

    ver 4

--- main1.cpp
+++ main1.cpp
@@ -31,7 +31,6 @@ while (std::cin >> next)
 }
 
 if (!std:cin)
-{#include <iostream>
 
 struct IntArray
 {
@@ -42,7 +41,64 @@ struct IntArray
 
  ~IntArray()
  {
+   delete[] a;#include <iostream>
 
+struct IntArray
+{
+ IntArray(int i)
+ {
+
+ }
+
+ ~IntArray()
+ {
+  delete[] a;
+ }
+
+ int add(int i);
+ int get(size_t id) const;
+ size_t size() const;
+ int last() const;
+
+ int* a;
+ size_t k;
+};
+
+int main()
+{
+ try
+ {
+  int next = 0;
+  std::cin >> next;
+
+  IntArray a(next);
+
+  while (std::cin >> next)
+  {
+   a.add(next);
+  }
+
+  if (!std::cin && !std::cin.eof())
+  {
+   return 1;
+  }
+
+  size_t count = 1;
+
+  for (size_t i = 0; i < a.size() - 1; ++i)
+  {
+   int d = a.get(i);
+
+   count += !(d % a.last()) ? 1 : 0;
+  }
+
+  std::cout << count << "\n";
+ }
+ catch (...)
+ {
+  return 1;
+ }
+}
  }
 
  int add(int i);
@@ -95,4 +151,67 @@ for (size_t i = 0, i < a.size(); - 1; ++i)
    }
  std::cout << count << "\n";
 
+}
+
+catch (...)
+{
+  a.
+}
+#include <iostream>
+
+struct IntArray
+{
+ IntArray(int i)
+ {
+
+ }
+
+ ~IntArray()
+ {
+  delete[] a;
+ }
+
+ int add(int i);
+ int get(size_t id) const;
+ size_t size() const;
+ int last() const;
+
+ int* a;
+ size_t k;
+};
+
+int main()
+{
+ try
+ {
+  int next = 0;
+  std::cin >> next;
+
+  IntArray a(next);
+
+  while (std::cin >> next)
+  {
+   a.add(next);
+  }
+
+  if (!std::cin && !std::cin.eof())
+  {
+   return 1;
+  }
+
+  size_t count = 1;
+
+  for (size_t i = 0; i < a.size() - 1; ++i)
+  {
+   int d = a.get(i);
+
+   count += !(d % a.last()) ? 1 : 0;
+  }
+
+  std::cout << count << "\n";
+ }
+ catch (...)
+ {
+  return 1;
+ }
 }
